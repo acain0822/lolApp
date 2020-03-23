7e258ef0a11d2e779a3dd8be207df42e2fbeed3b
tony-cain <cain.papercut@gmail.com>
2020-03-22 21:00:58 -0400

    add npm scripts and build

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-// var lolApp = require("flickity");
+$('.schedule-carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    groupCells: 4,
+    prevNextButtons: false,
+    pageDots: false
+  });
