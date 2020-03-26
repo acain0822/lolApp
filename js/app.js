43a3a206fbbd7dd75e0ca608001e422085285862
tony-cain <cain.papercut@gmail.com>
2020-03-26 19:38:15 -0400

    creating app.js to start data collection through pandascore

---
+++ js/app.js
@@ -0,0 +1,10 @@
+curl https://api.pandascore.co/lives?token=GD7lXcl8dyVTiJfgFM4TDj5wK5eXZJSHGE1LLmovKHQpg-A4uLg
+
+HTTP/1.1 200 OK
+Content-Type: application/json; charset=utf-8
+Link: <https://api.pandascore.co/lol/champions?page=3&token=GD7lXcl8dyVTiJfgFM4TDj5wK5eXZJSHGE1LLmovKHQpg-A4uLg>; rel="last", <https://api.pandascore.co/lol/champions?page=2&token=GD7lXcl8dyVTiJfgFM4TDj5wK5eXZJSHGE1LLmovKHQpg-A4uLg>; rel="next"
+X-Page: 1
+X-Per-Page: 50
+X-Request-Id: c51e8cf0-c397-421c-8a09-44c126f55a36
+X-Runtime: 0.077614
+X-Total: 133
