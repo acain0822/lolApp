f10284a92807b7a5e392ebc157811bd4e65dab4a
tony-cain <cain.papercut@gmail.com>
2020-03-28 10:53:38 -0400

    added data

--- js/app.js
+++ js/app.js
@@ -1,24 +1,33 @@
-// curl https://api.pandascore.co/lives?token=GD7lXcl8dyVTiJfgFM4TDj5wK5eXZJSHGE1LLmovKHQpg-A4uLg
 
-// HTTP/1.1 200 OK
-// Content-Type: application/json; charset=utf-8
-// Link: <https://api.pandascore.co/lol/champions?page=3&token=GD7lXcl8dyVTiJfgFM4TDj5wK5eXZJSHGE1LLmovKHQpg-A4uLg>; rel="last", <https://api.pandascore.co/lol/matches?page=2&token=GD7lXcl8dyVTiJfgFM4TDj5wK5eXZJSHGE1LLmovKHQpg-A4uLg>; rel="next"
-// X-Page: 1
-// X-Per-Page: 50
-// X-Request-Id: c51e8cf0-c397-421c-8a09-44c126f55a36
-// X-Runtime: 0.077614
-// X-Total: 133
-
-// const apiToken = 'GD7lXcl8dyVTiJfgFM4TDj5wK5eXZJSHGE1LLmovKHQpg-A4uLg';
-
-// var socket = new WebSocket('wss://live.pandascore.co/lol/matches/&token=GD7lXcl8dyVTiJfgFM4TDj5wK5eXZJSHGE1LLmovKHQpg-A4uLg');
-// socket.onmessage = function(event) {
-//     console.log(JSON.parse(event.data))
-// }
 
 var request = new XMLHttpRequest()
 
-request.open('GET', 'https://api.pandascore.co/lol/matches?&token=GD7lXcl8dyVTiJfgFM4TDj5wK5eXZJSHGE1LLmovKHQpg-A4uLg', 'true')
+var args = [
+    {
+    "begin_at": "2020-01-20T23:00:00Z",
+    "description": null,
+    "end_at": null,
+    "full_name": "Spring 2020",
+    "id": 2402,
+    "league": {},
+    "league_id": 4302,
+    "modified_at": "2020-01-20T17:51:43Z",
+    "name": null,
+    "season": "Spring",
+    "slug": "league-of-legends-prime-league-pro-division-spring-2020",
+    "tournaments": [],
+    "videogame": {
+    "id": 1,
+    "name": "LoL",
+    "slug": "league-of-legends"
+    },
+    "winner_id": null,
+    "winner_type": null,
+    "year": 2020
+    }
+    ];
+    
+request.open('GET', 'https://api.pandascore.co/lol/leagues?&token=GD7lXcl8dyVTiJfgFM4TDj5wK5eXZJSHGE1LLmovKHQpg-A4uLg', 'true')
 
 request.onload = function() {
     var data = JSON.parse(this.response)
