2abf5e971a56a7b78607a7f05fe2958fa2ce2926
tony-cain <cain.papercut@gmail.com>
2020-06-13 13:19:52 -0400

    pulled out schedule utility and created schedule ui component

--- leage-app/src/components/ScheduleComponent.js
+++ leage-app/src/components/ScheduleComponent.js
@@ -1,67 +1,44 @@
-import React, { Component } from "react";
-import moment from "moment";
+import React, { Component, Fragment } from "react";
+import { makeWeeksOfMatches } from "./utils";
+import { Table, TabContent, TabPane, Nav, NavItem, NavLink } from "reactstrap";
 
-function DisplayAllDates({ matches }) {
-  const allMatches = matches.map((match) => match.begin_at);
-
-  return JSON.stringify(allMatches);
-}
-
-function ParseSchedule({ matches }) {
-  // console.log(matches);
-  let matchDates = Array.from(matches.map((match) => match.begin_at));
-  // console.log(matchDates);
-
-  // from https://stackoverflow.com/questions/48083728/group-dates-by-week-javascript
-
-  //make an object that groups matches to weeks
-  // Week: matches
-  const groups = matchDates.reduce((acc = 0, date) => {
-    const yearWeek = `${moment(date).year()}-${moment(date).week()}`;
-
-    if (!acc[yearWeek]) {
-      acc[yearWeek] = [];
-    }
-
-    acc[yearWeek].push(date);
-    // acc[yearWeek].push(matchedMatch);
-    return acc;
-  }, {});
-
-// map an array of dates
-//getting dates that we will replace
-// ie ["2018-07-29T20:41:28Z", "2018-07-29T21:32:14Z", "2018-08-05T01:08:11Z"]
-  Object.values(groups).map((matchDateTime) => {
-  
-    console.log(matchDateTime);
-
-    //loop over every date in the array
-    for (let d = 0; d < matchDateTime.length; d++) {
-
-      //set a variable and set it to find a match from the matches array
-      //that has the same date and time to the
-      //entry in the matchDateTime array
-      console.log(matchDateTime[d]);
-      let matchWithDate = matches.filter( match => {
-       if(match.begin_at === matchDateTime[d]){
-         matchDateTime[d] = match;
-       }
-         console.log(matchDateTime[d]);
-
-      }
+function CreateSchedule({ listofMatches }) {
+  const weekArray = Object.keys(listofMatches);
+  const matchesofWeek = weekArray.map((week, inc = 0) => {
+    let weekObject = listofMatches[week];
+    let matches = weekObject.map((match) => {
+      console.log(match);
+      return (
+        <tr key={match.id}>
+          <td>
+            <img
+              alt={match.opponents[0].opponent.acronym}
+              style={{ width: "30px" }}
+              src={match.opponents[0].opponent.image_url}
+            />
+            {match.opponents[0].opponent.acronym} vs{" "}
+            <img
+            alt={match.opponents[1].opponent.acronym}
+              style={{ width: "30px" }}
+              src={match.opponents[1].opponent.image_url}
+            />
+            {match.opponents[1].opponent.acronym}
+          </td>
+          <td></td>
+        </tr>
       );
-
-    }
-    
-   // console.log(groups);
+    });
+    return (
+      <React.Fragment>
+        <thead>
+          <th>Week {inc + 1}</th>
+        </thead>
+        <tbody>{matches}</tbody>
+      </React.Fragment>
+    );
   });
 
-  console.log(groups);
-  var weeks = Object.keys(groups);
-  return weeks.map((week, index = 0) => {
-    index++;
-    return <div> Week {index}</div>;
-  });
+  return <Table dark hover responsive>{matchesofWeek}</Table>;
 }
 
 class Schedule extends Component {
@@ -72,10 +49,11 @@ class Schedule extends Component {
   render() {
     return (
       <React.Fragment>
-        <DisplayAllDates matches={this.props.matches} />
+        <CreateSchedule
+          listofMatches={makeWeeksOfMatches(this.props.matches)}
+        />
         <br />
         <br />
-        <ParseSchedule matches={this.props.matches} />
         <div>this is a schedule component</div>
       </React.Fragment>
     );
